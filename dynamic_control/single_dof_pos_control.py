8bc4b6e7c490a3c00d4fd75de9038282780966cc
DDoch98 <wnghks6884@naver.com>
2023-08-09 10:42:52 +0900

    dynamic control python api

---
+++ dynamic_control/single_dof_pos_control.py
@@ -0,0 +1,8 @@
+from omni.isaac.dynamic_control import _dynamic_control
+import numpy as np
+dc = _dynamic_control.acquire_dynamic_control_interface()
+articulation = dc.get_articulation("/UR16e")
+dc.wake_up_articulation(articulation)
+dof_ptr = dc.find_articulation_dof(articulation, "shoulder_lift_joint")
+# -1.5 rad = -85 degree
+dc.set_dof_position_target(dof_ptr,-1.5)
